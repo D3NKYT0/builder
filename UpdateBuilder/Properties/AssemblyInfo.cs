d7e0477a70b78afbd35f2a0aeafb34aa29e3e6a3
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-08-15 15:55:45 -0300

    ajuste

---
+++ UpdateBuilder/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Windows;
+
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyTitle("L2JCore Builder")]
+[assembly: AssemblyDescription("L2JCore Update Builder")]
+[assembly: AssemblyConfiguration("L2JCore Update Builder")]
+[assembly: AssemblyCompany("L2JCore Update Builder")]
+[assembly: AssemblyProduct("L2JCore Update Builder")]
+[assembly: AssemblyCopyright("L2jcore.top Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyFileVersion("1.7.2.35")]
+[assembly: AssemblyVersion("1.7.2.35")]
